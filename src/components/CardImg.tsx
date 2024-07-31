02e601e4cb01b7fb64f8b6d149dd258dabb44a63
Kalizar <kalinangelov@abv.bg>
2024-07-31 13:06:20 +0300

    Create CardImg.tsx

---
+++ src/components/CardImg.tsx
@@ -0,0 +1,29 @@
+const CardImg = () => {
+  return (
+    <div className="card-image">
+
+        <div className="card-back">
+            <p>000</p>
+        </div>
+
+        <div className="card-frond">
+            
+            <figure className="card-logo">
+                <img src="/public/images/card-logo.svg" alt="card logo" />
+            </figure>
+
+            <h1 className="card-number">0000 0000 0000 0000</h1>
+
+            <article>
+                <h2>Jane Applessed</h2>
+
+                <p>00/00</p>
+            </article>
+
+        </div>
+
+    </div>
+  );
+};
+
+export default CardImg;
