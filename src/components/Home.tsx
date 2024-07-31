c3e5c5156eb3a72dbfdb89f63076223b3735e18a
Kalizar <kalinangelov@abv.bg>
2024-07-31 13:06:23 +0300

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,79 +1,22 @@
 import { useState } from "react";
 
+import Form from "./Form";
+import CardImg from "./CardImg";
+import Message from "./Message";
 
 const Home = () => {
     const [cardNumber, setCardNumber] = useState<number>(0)
+    const [compleat, setcompleat] = useState<boolean>(false);
 
     return (
         <main>
-            <div className="card-image">
 
-                <figure className="card-back">
-                    <img src="/images/bg-card-back.png" alt="card back" />
-                </figure>
+            <CardImg />
 
-                <section className="card-information">
-                    
-                    <figure className="card-logo">
-                        <img src="/public/images/card-logo.svg" alt="card logo" />
-                    </figure>
-
-                    <h1 className="card-number">0000 0000 0000 0000</h1>
-
-                    <article>
-                        <h2>Jane Applessed</h2>
-
-                        <p>{cardNumber}</p>
-                    </article>
-
-                </section>
-            </div>
-
-            <form>
-
-                <fieldset>
-                    <label htmlFor="card">Card holder name</label>
-                    <input 
-                        type="text" 
-                        id="card" 
-                        placeholder="e.g. Jane Applessed" 
-                    />
-                </fieldset>
-
-                <fieldset>
-                    <label htmlFor="number">card number</label>
-                    <input 
-                        type="tel"
-                        inputMode="numeric"
-                        pattern="[0-9\s]{13,19}"
-                        maxLength={19}
-                        required
-                        id="number" 
-                        placeholder="e.g. 1234 5678 9123 0000"
-                    />
-                </fieldset>
-
-                <fieldset>
-                    <label htmlFor="date">ext. date (mm/yy) cvc</label>
-                    <input 
-                        type="number" 
-                        id="date" 
-                        placeholder="MM"
-                    />
-                    <input 
-                        type="number" 
-                        id="date" 
-                        placeholder="YY"
-                    />
-                    <input 
-                        type="number" 
-                        id="date" 
-                        placeholder="e.g. 123"
-                    />
-                </fieldset>
-                
-                <button type="submit">Confirm</button>
-            </form>
+            {
+                compleat ? <Message /> : <Form />
+            }
+    
         </main>
     );
 };
