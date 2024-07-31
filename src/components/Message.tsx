2b940ddca326fa4cc46a2dc48cc16488074d5161
Kalizar <kalinangelov@abv.bg>
2024-07-31 13:06:25 +0300

    Create Message.tsx

---
+++ src/components/Message.tsx
@@ -0,0 +1,21 @@
+const Message = () => {
+  return (
+
+    <section className="message">
+
+        <figure>
+            <img src="/images/icon-complete.svg" alt="check" />
+        </figure>
+
+        <h2>Thank you!</h2>
+
+        <p>We,ve added your card details</p>
+
+        <button>Continue</button>
+
+    </section>
+
+  );
+};
+
+export default Message;
