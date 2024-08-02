3a1c053e9dfdc4ad7ca00ca9ae82dab170cf4f7c
Kalizar <kalinangelov@abv.bg>
2024-08-02 11:07:15 +0300

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 const Form = () => {
     const [errorMes, setErrorMes] = useState<string>("");
     const [cardHolder, setCardHolder] = useState<string>("");
-    const [cardNumber, setCardNumber] = useState<number>(0);
-    const [cadDate, setCardDate] = useState<number>(0);
-    const [cardCVC, setCardCVC] = useState<number>(0);
+    const [cardNumber, setCardNumber] = useState<number | string>("");
+    const [cardDateMonth, setCardDateMonth] = useState<number | string>("");
+    const [cardDateYear, setCardDateYear] = useState<number | string>("");
+    const [cardCVC, setCardCVC] = useState<number | string>("");
 
+    const onSubmit = (e :FormEvent) => {
+        e.preventDefault();
+        console.log(cardHolder);
+        console.log(cardNumber);
+        console.log(cardDateMonth);
+        console.log(cardDateYear);
+        console.log(cardCVC);
+    };
+
+ 
     return (
         <form>
 
@@ -31,6 +42,8 @@ const Form = () => {
                     maxLength={16}
                     id="number" 
                     placeholder="e.g. 1234 5678 9123 0000"
+                    value={cardNumber}
+                    onChange={(e) => setCardNumber(e.target.value)}
                 />
                 <p className="error">Error</p>
             </fieldset>
@@ -46,17 +59,21 @@ const Form = () => {
 
                     <input 
                         type="tel" 
-                        id="mm-yy" 
+                        id="mm" 
                         placeholder="MM"
                         maxLength={2}
                         inputMode="numeric"
+                        value={cardDateMonth}
+                        onChange={(e) => setCardDateMonth(e.target.value)}
                     />
                     <input 
                         type="tel" 
-                        id="mm-yy" 
+                        id="yy" 
                         placeholder="YY"
                         maxLength={2}
                         inputMode="numeric"
+                        value={cardDateYear}
+                        onChange={(e) => setCardDateYear(e.target.value)}
                     />
 
                     <input 
@@ -65,6 +82,8 @@ const Form = () => {
                         placeholder="e.g. 123"
                         maxLength={3}
                         inputMode="numeric"
+                        value={cardCVC}
+                        onChange={(e) => setCardCVC(e.target.value)}
                     />
 
                 </div>
@@ -77,7 +96,7 @@ const Form = () => {
                 
             </fieldset>
             
-            <button type="submit">Confirm</button>
+            <button type="submit" onSubmit={onSubmit}>Confirm</button>
 
         </form>
     );
