ab39e0b3ab840b80cc401c15a1ee3913a177a42b
Kalizar <kalinangelov@abv.bg>
2024-08-03 14:35:35 +0300

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,55 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState, useContext, FC } from "react";
+
+import { FormState } from "../types/formState";
+import { AppProviderProps } from "../types/appProviderProps";
+import { Context } from "../context/Context";
 
 import Form from "./Form";
 import CardImg from "./CardImg";
 import Message from "./Message";
 
-const Home = () => {
+const Home: FC<AppProviderProps> = () => {
     const [cardNumber, setCardNumber] = useState<number>(0)
     const [compleat, setcompleat] = useState<boolean>(false);
+    const [formData, setFormData] = useState<FormState>({
+        cardHolder: "",
+        cardNumber: "",
+        cardDateMonth: "",
+        cardDateYear: "",
+        cardCVC: ""
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((data) => ({
+            ...data,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e :FormEvent) => {
+        e.preventDefault();
+    };
+
+    const initialValue = {
+        formData, 
+        setFormData, 
+        handleChange, 
+        handleSubmit,
+    };
 
     return (
-        <main>
+        <Context.Provider value={initialValue}>
+            <main>
 
-            <CardImg />
+                <CardImg />
 
-            {
-                compleat ? <Message /> : <Form />
-            }
-    
-        </main>
+                {
+                    compleat ? <Message /> : <Form />
+                }
+        
+            </main>
+        </Context.Provider>
     );
 };
 
