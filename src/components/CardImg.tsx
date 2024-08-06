5b4ab91f9e37f58e40334889f92ded8ece9092dc
Kalizar <kalinangelov@abv.bg>
2024-08-06 13:32:51 +0300

    Update CardImg.tsx

--- src/components/CardImg.tsx
+++ src/components/CardImg.tsx
@@ -1,29 +1,39 @@
-const CardImg = () => {
-  return (
-    <div className="card-image">
+import { User } from "../types/formType";
 
-        <div className="card-back">
-            <p>000</p>
+const CardImg = ({ formData }: User) => {
+    
+    const {cardHolder, cardNumber, cardDateMonth, cardDateYear, cardCVC} = formData;
+    const num = String(cardNumber);
+    const cardNum = `${num.slice(0, 4)} ${num.slice(4, 8)} ${num.slice(8, 12)} ${num.slice(12, 16)}`;  
+    
+    return (
+        <div className="card-image">
+
+            <div className="card-back">
+
+                <p>{cardHolder === "" ? "000" : cardCVC}</p>
+                
+            </div>
+
+            <div className="card-frond">
+                
+                <figure className="card-logo">
+                    <img src="/public/images/card-logo.svg" alt="card logo" />
+                </figure>
+
+                <h1 className="card-number">{cardNumber === "" ? "0000 0000 0000 0000" : cardNum}</h1>
+
+                <article>
+                   
+                    <h2> {cardHolder === "" ? "Jane Applessed": cardHolder}</h2>
+
+                    <p>{cardDateMonth === "" ? "00" : cardDateMonth}/{cardDateYear === "" ? "00" : cardDateYear}</p>
+                </article>
+
+            </div>
+
         </div>
-
-        <div className="card-frond">
-            
-            <figure className="card-logo">
-                <img src="/public/images/card-logo.svg" alt="card logo" />
-            </figure>
-
-            <h1 className="card-number">0000 0000 0000 0000</h1>
-
-            <article>
-                <h2>Jane Applessed</h2>
-
-                <p>00/00</p>
-            </article>
-
-        </div>
-
-    </div>
-  );
+    );
 };
 
 export default CardImg;
