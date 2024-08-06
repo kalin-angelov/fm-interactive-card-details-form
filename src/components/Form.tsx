f89495b75958822c8bdb28a68195acc7356bcf5c
Kalizar <kalinangelov@abv.bg>
2024-08-06 13:32:49 +0300

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,35 @@
-import { useState, useContext } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-const Form = () => {
-    const [errorMes, setErrorMes] = useState<string>("");
+import { FormProps, FormState } from "../types/formType";
+
+
+const Form = ({ setCompleat, setFormData }: FormProps) => {
+
+    const { register, handleSubmit, formState: { errors }} = useForm<FormState>()
+
+    const onSubmit: SubmitHandler<FormState> = (data) => {
+        setFormData(data);
+        setCompleat(true);
+    };
  
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
 
             <fieldset className="card-user-field">
                 <label htmlFor="cardHolder">Card holder name</label>
                 <input 
                     type="text" 
                     id="cardHolder" 
-                    name="cardHolder"
                     placeholder="e.g. Jane Applessed" 
-                    value={formData.cardHolder}
-                    onChange={handleChange}
+                    {...register("cardHolder", { required: "Can't be blank", 
+                        pattern:{
+                            value: /^[A-Za-z]+ [A-Za-z]+$/gm,
+                            message: "Invalid name"
+                        }
+                    })}
+                    aria-invalid={errors.cardHolder ? "true" : "false"}
                 />
-                <p className="error">Error</p>
+                {errors.cardHolder && <p className="error">{errors.cardHolder.message}</p>}
             </fieldset>
             
 
@@ -24,15 +37,27 @@ const Form = () => {
                 <label htmlFor="cardNumber">card number</label>
                 <input 
                     type="tel"
-                    inputMode="numeric"
-                    maxLength={16}
                     id="cardNumber" 
-                    name="cardNumber"
                     placeholder="e.g. 1234 5678 9123 0000"
-                    value={formData.cardNumber}
-                    onChange={handleChange}
+                    maxLength={16}
+                    {...register("cardNumber", {
+                        required: "Can't be blank", 
+                        pattern: {
+                            value:/^[0-9]+$/gm,
+                            message: "Wrong format, numbers only"
+                        },
+                        maxLength: {
+                            value: 16,
+                            message: "Can't be less or more than 16 digits"
+                        },
+                        minLength: {
+                            value: 16,
+                            message: "Can't be less or more than 16 digits"
+                        },
+                    })}
+                    aria-invalid={errors.cardHolder ? "true" : "false"}
                 />
-                <p className="error">Error</p>
+                {errors.cardNumber && <p className="error">{errors.cardNumber.message}</p>}
             </fieldset>
            
             <fieldset className="card-date-field">
@@ -44,46 +69,91 @@ const Form = () => {
 
                 <div className="inputs">
 
-                    <input 
-                        type="tel" 
-                        id="mm" 
-                        name="cardDateMonth"
-                        placeholder="MM"
-                        maxLength={2}
-                        inputMode="numeric"
-                        value={formData.cardDateMonth}
-                        onChange={handleChange}
-                    />
-                    <input 
-                        type="tel" 
-                        id="yy" 
-                        name="cardDateYear"
-                        placeholder="YY"
-                        maxLength={2}
-                        inputMode="numeric"
-                        value={formData.cardDateYear}
-                        onChange={handleChange}
-                    />
+                    <fieldset className="date">
+                        <input 
+                            type="tel" 
+                            id="mm-yy" 
+                            placeholder="MM"
+                            maxLength={2}
+                            {...register("cardDateMonth", { 
+                                required: "Can't be blank", 
+                                pattern: {
+                                    value:/^[0-9]+$/gm,
+                                    message: "Wrong format, numbers only"
+                                },
+                                maxLength: {
+                                    value: 2,
+                                    message: "Can't be less or more than 2 digits"
+                                },
+                                minLength: {
+                                    value: 2,
+                                    message: "Can't be less or more than 2 digits"
+                                },
+                            })}
+                            aria-invalid={errors.cardDateMonth ? "true" : "false"}
+                        />
 
-                    <input 
-                        type="tel" 
-                        id="cvc" 
-                        name="cardCVC"
-                        placeholder="e.g. 123"
-                        maxLength={3}
-                        inputMode="numeric"
-                        value={formData.cardCVC}
-                        onChange={handleChange}
-                    />
+                        {errors.cardDateMonth && <p className="error">{errors.cardDateMonth.message}</p>}
 
+                    </fieldset>
+                    
+                    <fieldset className="date">
+                        <input 
+                            type="tel" 
+                            id="mm-yy" 
+                            placeholder="YY"
+                            maxLength={2}
+                            {...register("cardDateYear", {
+                                required: "Can't be blank", 
+                                pattern: {
+                                    value:/^[0-9]+$/gm,
+                                    message: "Wrong format, numbers only"
+                                },
+                                maxLength: {
+                                    value: 2,
+                                    message: "Can't be less or more than 2 digits"
+                                },
+                                minLength: {
+                                    value: 2,
+                                    message: "Can't be less or more than 2 digits"
+                                },
+                            })}
+                            aria-invalid={errors.cardDateYear ? "true" : "false"}
+                        />
+
+                        {errors.cardDateYear && <p className="error">{errors.cardDateYear.message}</p>}   
+
+                    </fieldset>
+                    
+                    <fieldset className="date">
+                        <input 
+                            type="tel" 
+                            id="cvc"
+                            placeholder="e.g. 123"
+                            maxLength={3}
+                            {...register("cardCVC", { 
+                                required: "Can't be blank", 
+                                pattern: {
+                                    value:/^[0-9]+$/gm,
+                                    message: "Wrong format, numbers only"
+                                },
+                                maxLength: {
+                                    value: 3,
+                                    message: "Can't be less or more than 3 digits"
+                                },
+                                minLength: {
+                                    value: 3,
+                                    message: "Can't be less or more than 3 digits"
+                                },
+                            })}
+                            aria-invalid={errors.cardCVC ? "true" : "false"}
+                        />
+
+                        {errors.cardCVC && <p className="error">{errors.cardCVC.message}</p>}
+                    </fieldset>
+                    
                 </div>
 
-                <div className="date-input-error">
-                    <p className="error">Error</p>
-                    <p className="error">Error</p>
-                    <p className="error">Error</p>
-                </div>
-                
             </fieldset>
             
             <button type="submit">Confirm</button>
